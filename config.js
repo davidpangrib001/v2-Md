9bd78ff75096495a1fa4dd438fb2caffda86f493
OneBotz Owner <96653161+davidpangrib001@users.noreply.github.com>
2022-05-06 08:40:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 
-global.owner = ['6288279268363'] // Put your number here
-global.mods = [] // Want some help?
+global.owner = ['6282160268332','6282128475388'] // Put your number here
+global.mods = ['6282160268332'] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -20,40 +20,40 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'https://pastelink.net/v20Md',
-  'https://api.lolhuman.xyz': 'rey2k21',
+  'https://api.xteam.xyz': 'cristian9407',
+  'https://api.lolhuman.xyz': '7047f08f5a0ea4bcdf55cd72',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.me': 'apivinz',
   'https://pencarikode.xyz': 'pais',
-  'https://apikey-bear3.herokuapp.com': 'https://pastelink.net/v20Md',
+  'https://apikey-bear3.herokuapp.com': 'KingOfBear',
   'https://viko-api.herokuapp.com': 'vinko',
   'https://leyscoders-api.herokuapp.com': 'dappakntlll'
 }
 
-lolkey = 'rey2k21'
+lolkey = '7047f08f5a0ea4bcdf55cd72''
 zekskey = 'apivinz'
-xteamkey = 'https://pastelink.net/v20Md'
+xteamkey = 'cristian9407'
 leyskey = 'dappakntlll'
-bearkey = 'https://pastelink.net/v20Md'
-ApiiKey = 'https://pastelink.net/v20Md'
+bearkey = 'KingOfBear'
+ApiiKey = 'apivinz'
 vinkey = 'vinko'
 
-namabot = 'Yuki Bot'
-namalu = 'King Of Bear'
-footer = '\n©King Of Bear'
+namabot = 'One Bot'
+namalu = 'David'
+footer = '\nOne Bot by David'
 kasihcaption = `Nih Sayang`
 
 // Sticker WM
 global.stiker_wait = '_*Tunggu Sebentar...*_'
-global.packname = 'YukiBot Multi device'
-global.author = 'King Of Bear'
-global.email = 'drakipul1016@gmail.com'
+global.packname = 'One Bot Multi device'
+global.author = 'David'
+global.email = 'davidpangrib001@gmail.com'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wm = '©King Of Bear Bot-Md'
-global.media = 'https://telegra.ph/file/5d45a3d1e12726d98c5e4.jpg'
+global.wm = 'One BotZ MD'
+global.media = 'https://telegra.ph/file/e2f22aa820cee9810a8f4.jpg'
 
 global.wait = '_*Tunggu Sebentar Sedang Di Proses...*_'
-global.eror = '_*Server Error*_'
+global.eror = '_*Terjadi kesalahan*_'
 
 global.multiplier = 69 // The higher, The harder levelup
 
