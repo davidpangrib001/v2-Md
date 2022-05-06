c0420bd5a60c05f335e019d7541071665ceb6d7b
OneBotz Owner <96653161+davidpangrib001@users.noreply.github.com>
2022-05-06 08:42:25 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,12 +2,12 @@ const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
-                    + 'N:;KingOfBear;;;'
-                    + 'FN:KingOfBear\n' // full name
-                    + 'ORG:Owner YukiBot;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6288279268363:+6288279268363\n' // WhatsApp ID + phone number
+                    + 'N:;Seseorang;;;'
+                    + 'FNYang Bernama David\n' // full name
+                    + 'ORG:Dia Bocil Bang;\n' // the organization of the contact
+                    + 'TEL;type=CELL;type=VOICE;waid=6282160268332:+6282160268332\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
-                conn.sendMessage(m.chat, { contacts: { displayName: 'King Of Bear', contacts: [{ vcard }] } }, { quoted: m })
+                conn.sendMessage(m.chat, { contacts: { displayName: 'Davitt', contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
